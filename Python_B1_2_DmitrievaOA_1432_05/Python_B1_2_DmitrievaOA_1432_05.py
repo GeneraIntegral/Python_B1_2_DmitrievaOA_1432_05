2b81f1f468d593ef17141df1daff00571b36494d
User <User@WIN-NCFEC32DCAH>
2023-06-05 14:49:54 +0300

    Добавьте файлы проекта.

---
+++ Python_B1_2_DmitrievaOA_1432_05/Python_B1_2_DmitrievaOA_1432_05.py
@@ -0,0 +1,12 @@
+﻿# Python_B1_2_DmitrievaOA_1432_05
+
+s = input("Enter the string: ") # Введите строку:
+
+for i in range(len(s)):
+    if i == 2:
+        continue
+    if s[i] == 'c':
+        print(" Found symbol c") # Найден символ c:
+    print(s[i], end='')
+
+print("\nLine length:", len(s)-1) # Длина строки:
